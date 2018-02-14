3ad6990795d90de4769de05a6a7808bbc53721a7
CS <cs@wahdis.com>
2018-02-13 22:40:23 -0500

    Sync with Dropbox

--- sync.sh
+++ sync.sh
@@ -1,5 +1,29 @@
 #!/bin/bash -e
 
+TARGET=Dropbox/encrypted
+SOURCEBASE=~/virtual/dropbox-common/
+SOURCE="${SOURCEBASE}${TARGET}"
+DESTHOST="chris@localhost"
+DESTBASE=/home/chris/
+DEST="${DESTHOST}:${DESTBASE}${TARGET}"
 
+upload_vm () {
+    rsync -e "ssh -p 2222" -apP --del "${SOURCE}/" "${DEST}"
+}
 
-https://www.dropbox.com/help/desktop-web/linux-commands
+run_dropbox () {
+    ssh -p 2222 -q chris@localhost <<'EOF'
+    dropbox running || { dropbox stop; pgrep -f dropbox | xargs -r kill; }
+    dropbox start 2> /dev/null
+    STATUS=$(dropbox status)
+    until [ "${STATUS}" == "Up to date" ] ; do
+        echo $STATUS
+        sleep 5
+        STATUS=$(dropbox status)
+    done
+    dropbox stop
+EOF
+}
+
+upload_vm
+run_dropbox
